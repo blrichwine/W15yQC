eb9ee52307cbcc8636a62dea90b3313d23c0548f
Brian Richwine <blrichwine@gmail.com>
2012-12-20 01:02:14 -0500

    Updated version number to 1.0 Beta 13

--- content/overlay.js
+++ content/overlay.js
@@ -34,7 +34,7 @@ if (!blr) { var blr = {}; }
  */
 if (!blr.W15yQC) {
   blr.W15yQC = {
-    version: '1.0-B12',
+    version: '1.0-B13',
     // Following are variables for setting various options:
     bHonorARIAHiddenAttribute: true,
     bHonorCSSDisplayNoneAndVisibilityHidden: true,
